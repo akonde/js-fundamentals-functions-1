9d65fe100b8e177c30351c8430d8064f9bb3393f
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-04-26 08:55:13 +0100

    function exercise completed!

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,7 +7,10 @@
 // 2      | 3
 //
 // TODO: write code below
-
+function incrementFunc(number) {
+  return number + 1
+}
+console.log(incrementFunc(5))
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,6 +21,11 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalizeFunc(word) {
+  const str = word.charAt(0).toUpperCase() + word.slice(1)
+  return str
+}
+console.log(capitalizeFunc('taYo'))
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,11 +37,16 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function personEmog(name) {
+  const result = 'Hi, ' + capitalizeFunc(name) + ' ' + ':)'
+  return result
+}
+console.log(personEmog('ade'))
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
-//
+// function
 // Input                | Output
 // ['edward']           | 1
 // ['edward', 'aiyana'] | 2
@@ -42,7 +55,20 @@
 //
 // TODO: write code below
 
-// 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
+function arrayStr(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      console.log(typeof arr[i])
+      count++
+    }
+  }
+  return count
+}
+console.log(arrayStr(['ade', 4, 'love']))
+
+// 5. Define a function that takes an object and adds a property 'edward'
+//  to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
 //
@@ -50,14 +76,22 @@
 // {}                       | { edward: 'amazing' }
 // { aiyana: 'fantastic' }  | { aiyana: 'fantastic', edward: 'amazing' }
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
-//
+
 // TODO: write code below
+function objectProp(obj) {
+  if (obj.edward === undefined) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
+const myObj = {}
+console.log(objectProp(myObj))
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementFunc, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalizeFunc, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: personEmog, // etc
+  d: arrayStr,
+  e: objectProp
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,7 +9,14 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
-
+function twoNums(lower, upper) {
+  const results = []
+  for (let i = lower; i <= upper; i++) {
+    results.push(i)
+  }
+  return results
+}
+console.log(twoNums(1, 3))
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,9 +28,18 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function twoArg(str, num) {
+  let text = str.toUpperCase()
 
+  for (let i = 0; i < num; i++) {
+    text = text + '!'
+  }
+  return text
+}
+console.log(twoArg('ade', 3))
 // 3. define a function that takes two arguments: a string and a number.
-// The function should return the new time in 24hr time after adding the additional time in minutes.
+// The function should return the new time in 24hr time after
+//  adding the additional time in minutes.
 //
 // Example Input and Output:
 // Input        | Output
@@ -34,9 +50,29 @@
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+function timeIn24hr(str, num) {
+  const [hours, minutes] = str.split(':').map(Number)
+
+  // Calculate total minutes
+  let totalMinutes = hours * 60 + minutes + num
+
+  // Normalize total minutes to stay within a day (24 hours)
+  totalMinutes %= 1440
+
+  // Calculate new hours and minutes
+  const newHours = Math.floor(totalMinutes / 60)
+  const newMinutes = totalMinutes % 60
+
+  // Format new time as a string in 24-hour format
+  const newTime = `${newHours.toString().padStart(2, '0')}:${newMinutes
+    .toString()
+    .padStart(2, '0')}`
+
+  return newTime
+}
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: twoNums, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: twoArg, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: timeIn24hr // etc
 }
